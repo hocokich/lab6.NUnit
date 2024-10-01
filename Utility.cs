ce3e3753d88d546ac250a32f9053c9045775fc98
Александр <bondarenco65@gmail.com>
2024-10-01 21:59:53 +0700

    final

--- Utility.cs
+++ Utility.cs
@@ -8,6 +8,29 @@ namespace lab6.NUnit
 {
     internal class Utility
     {
+        public int Convert(float num, string unit)
+        {
+            if (num <= 0 ) throw new ArgumentException("Число должно быть > 0");
 
+            if (unit == "inch")
+                return (int)(num * 2.5);
+            if (unit == "cm")
+                return (int)(num / 2.5);
+            else throw new ArgumentException("unit должен быть inch || cm");
+        }
+        public bool DefineOrOddNum(int num)
+        {
+            return num % 2 == 0;
+        }
+        public int BiggestNum(int[] num) 
+        {
+            if(num == null) throw new ArgumentException("Массив не должен быть null");
+            return num.Max();
+        }
+        public int Remainder(int a, int b)
+        {
+            if (b <= 0) throw new ArgumentException("Делитель должен быть > 0");
+            return a % b;
+        }
     }
 }
---
+++ UtilityTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6.NUnit
+{
+    // атрибут, указывающий на то, что это класс содержит тесты
+    [TestFixture]
+    internal class UtilityTest
+    {
+        Utility u = new Utility();
+        // атрибут, указывающий на то, что это тестовый метод
+        [TestCase]
+        public void TestConvert()
+        {
+            //Тест функции конвертации
+
+            // получение исключения
+            var exception = Assert.Throws<ArgumentException>(() => u.Convert(10, "qwerty"));
+            // сравнение полученного сообщения с ожидаемым
+            Assert.That(exception.Message, Is.EqualTo("unit должен быть inch || cm"));
+            // получение исключения отрицательного числа
+            var exception2 = Assert.Throws<ArgumentException>(() => u.Convert(-2, "cm"));
+            // сравнение полученного сообщения с ожидаемым
+            Assert.That(exception2.Message, Is.EqualTo("Число должно быть > 0"));
+
+            Assert.AreEqual(25, u.Convert(10, "inch"));
+            Assert.AreEqual(4, u.Convert(10, "cm"));
+        }
+        [TestCase]
+        public void TestDefineOrOddNum()
+        {
+            //Тест функции Чет/Нечет
+            Assert.IsTrue(u.DefineOrOddNum(4));
+            Assert.IsFalse(u.DefineOrOddNum(5));
+            Assert.IsFalse(u.DefineOrOddNum(-1));
+        }
+        [TestCase]
+        public void TestBiggestNum()
+        {
+            //Тест функции нахождения наибольшего числа
+            // получение исключения
+            int[] arr = null;
+            var exception = Assert.Throws<ArgumentException>(() => u.BiggestNum(arr));
+            // сравнение полученного сообщения с ожидаемым
+            Assert.That(exception.Message, Is.EqualTo("Массив не должен быть null"));
+            // проверка выполняется успешно, если исключение не было сгенерировано
+            Assert.DoesNotThrow(() => u.BiggestNum(new int[] { -1, 4, 15, 45, 5, 0, -4 }));
+        }
+        [TestCase]
+        public void TestRemainder()
+        {
+            //Тест функции нахождения остатка от деления
+            // получение исключения
+            var exception = Assert.Throws<ArgumentException>(() => u.Remainder(14, 0));
+            // сравнение полученного сообщения с ожидаемым
+            Assert.That(exception.Message, Is.EqualTo("Делитель должен быть > 0"));
+            // проверка выполняется успешно, если исключение не было сгенерировано
+            Assert.DoesNotThrow(() => u.Remainder(2, 1));
+        }
+    }
+}
